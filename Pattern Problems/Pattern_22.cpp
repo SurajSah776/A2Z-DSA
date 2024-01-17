00b2054ae31eb930f74e064066c0c95458cf6d2f
SurajSah776 <surajkumarsah2060@gmail.com>
2024-01-17 21:53:44 +0530

    hard problems added

---
+++ Pattern Problems/Pattern_22.cpp
@@ -0,0 +1,37 @@
+/*
+*** Pattern - 22 ***
+
+ * * * *
+ *     *
+ *     *
+ * * * *
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // Function to Print the Pattern
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ Pattern Problems/Pattern_23.cpp
@@ -0,0 +1,32 @@
+/*
+*** Pattern - 23 ***
+
+ 4 4 4 4 4 4 4
+ 4 3 3 3 3 3 4
+ 4 3 2 2 2 3 4
+ 4 3 2 1 2 3 4
+ 4 3 2 2 2 3 4
+ 4 3 3 3 3 3 4
+ 4 4 4 4 4 4 4
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // Function to Print the Pattern
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            
+        }
+    }
+
+    return 0;
+}
